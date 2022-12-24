2e127aaa332423b79bff2e321247010b5ee0a69f
sreyeesh <sgarime1@gmail.com>
2022-12-25 01:07:50 +0200

    Update frontEndFileCreator.py
    updated this code  to make into CLI. need to figure how to see if it works properly for CLI. The code overall works in creating the projects folder   python  command

--- frontEndFileCreator.py
+++ frontEndFileCreator.py
@@ -1,31 +1,44 @@
 import os
+import argparse
 
-#making directory structure for css and js
-if not os.path.exists("project/css"):
-    os.makedirs("project/css")
+
+# Create the argparse argument parser
+parser = argparse.ArgumentParser()
+
+# Add the project directory as a command line argument
+parser.add_argument("project_dir", help="the project directory")
+
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+# Create the css and js directories if they do not already exist
+if not os.path.exists(args.project_dir + "/css"):
+    os.makedirs(args.project_dir + "/css")
 else:
-    print("css directory already exists!")    
+    print("css directory already exists!")   
 
 
-if not os.path.exists("project/js"):
-    os.makedirs("project/js")
+if not os.path.exists(args.project_dir + "/js"):
+    os.makedirs(args.project_dir + "/js")
 else:
     print("js directory already exists!")
 
 #creating the style.css, app.js, if they don already exist
-if not os.path.exists("project/css/style.css"):
-    with open("project/css/style.css", "w") as f:
+if not os.path.exists(args.project_dir + "/index.html"):
+    with open(args.project_dir + "/index.html", "w") as f:
         f.write("<!DOCTYPE html>")
 else:
-    print("index.html directory already exists!")        
+    print("index.html file already exists!")        
 
-if not os.path.exists("project/css/style.css"):
-    open("project/css/style.css","w").close()
+
+if not os.path.exists(args.project_dir + "/css/style.css"):
+    open(args.project_dir + "/css/style.css","w").close()
 else:
     print("style.css file already exists")    
 
-if not os.path.exists("project/js/app.js"):
-    open("project/js/app.js","w").close()
+if not os.path.exists(args.project_dir + "/js/app.js"):
+    open(args.project_dir +"/js/app.js","w").close()
 else:
     print("app.js file already exists!")    
 
