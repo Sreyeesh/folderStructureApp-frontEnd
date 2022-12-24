bd036d4a399a569235b4f0ddd2c4ca6ab905f2b7
sreyeesh <sgarime1@gmail.com>
2022-12-24 23:44:28 +0200

    restructured from hardcoded to paths

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # folderStructureApp-frontEnd
  This python app makes folder strutures for front end developers so they don't have to manually create  index,css, js or other files.
+
+project
+├── css
+│   └── style.css
+├── js
+│   └── app.js
+└── index.html
+
+If the css, js, index.html, style.css, or app.js files or directories already exist, the script will skip creating them and display a message indicating that they already exist.
+
+The index.html file will also include the HTML doctype automatically.
+
+To use the script, simply run it using Python.
+
+I hope this helps! Let me know if you have any questions or if you need further assistance.
--- frontEndFileCreator.py
+++ frontEndFileCreator.py
@@ -1,92 +1,31 @@
 import os
-import shutil
 
-#making directory structure
-
-src = "src"
-frontEnd = "www"
-js_folder = "js"
-css_folder = "css"
-backEnd = "server"
-parent_dir = "D:\Projects\CodingProjects\personalProjects"
-
-indexFileName = "index.html"
-cssFileName = "style.css"
-jsFileName = "app.js"
-
-src_path = os.path.join(parent_dir,src)
-frontEnd_path = os.path.join(src_path,frontEnd)
-js_folder_path = os.path.join(frontEnd_path,js_folder)
-css_folder_path = os.path.join(frontEnd_path,css_folder)
-backEnd_path = os.path.join(src_path,backEnd)
-
-if not os.path.exists(src_path):
-	os.mkdir(src_path)
-print("Directory '% s' created" % src_path)
-if not os.path.exists(frontEnd_path):
-	os.mkdir(frontEnd_path)
-
-if not os.path.exists(js_folder_path):
-	os.mkdir(js_folder_path)
-if not os.path.exists(css_folder_path):
-	os.mkdir(css_folder_path)
-
-if not os.path.exists(backEnd_path):
-	os.mkdir(backEnd_path)
+#making directory structure for css and js
+if not os.path.exists("project/css"):
+    os.makedirs("project/css")
+else:
+    print("css directory already exists!")    
 
 
+if not os.path.exists("project/js"):
+    os.makedirs("project/js")
+else:
+    print("js directory already exists!")
 
-#file creation happens after  directories are made 	
+#creating the style.css, app.js, if they don already exist
+if not os.path.exists("project/css/style.css"):
+    with open("project/css/style.css", "w") as f:
+        f.write("<!DOCTYPE html>")
+else:
+    print("index.html directory already exists!")        
 
-indexFileName_path = os.path.join(frontEnd_path, indexFileName)
-try:
-	with open(indexFileName_path,'w') as html:
-		html.write("""!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-	</head>
-	<body>
-	</body>
-	</html>""")
-	
-except:
-	print("Folders are already created")	
-	# print(file_html)
-print('creating,{}!'.format(indexFileName)) # creating index.html
+if not os.path.exists("project/css/style.css"):
+    open("project/css/style.css","w").close()
+else:
+    print("style.css file already exists")    
 
-cssFileName_path = os.path.join(css_folder, cssFileName)
-
-try:
-	with open(cssFileName_path,'w') as css:
-		css.write()
-except:
-	print("Folders are already created")	
-	# print(file_html)
-print('creating,{}!'.format(cssFileName)) # creating cssFileName
-
-jsFileName_path = os.path.join(js_folder, jsFileName)
-try:
-	with open(jsFileName_path ,'w') as js:
-		js.write()
-except:
-	print("Folders are already created")	
-	# print(file_html)
-print('creating,{}!'.format(cssFileName)) # creating cssFileName
-
-
-# source = "D:\Projects\CodingProjects\personalProjects\src\www"
-
-# jsSourceFile = "D:\Projects\CodingProjects\personalProjects\src\js"
-# cssSourceFile = "D:\Projects\CodingProjects\personalProjects\src\css"
-
-# jsDest = shutil.move(source,jsSourceFile)
-# print('moving,{}!',format(jsSourceFile))
-# cssDest = shutil.move(source,cssSourceFile)
-# print('moving,{}!',format(cssDest))
-
-
+if not os.path.exists("project/js/app.js"):
+    open("project/js/app.js","w").close()
+else:
+    print("app.js file already exists!")    
 
